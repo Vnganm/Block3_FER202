91986dcef844ff7dd45ad60778108f67f74843a5
Vnganm <161715774+Vnganm@users.noreply.github.com>
2025-08-11 10:02:43 +0700

    add slot 11

---
+++ Slot1/slot1-ex1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  const myName = "Mai Ngân"; // Thay "Your Name" bằng tên của bạn
+
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <p>My name is {myName}</p>
+    </div>
+  );
+}
+
+export default App;
